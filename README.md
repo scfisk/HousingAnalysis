8751ad02a5db47e97b73b51fa1548391357e040c
Sara Nuss <35274866+scfisk@users.noreply.github.com>
2025-07-23 19:59:51 -0400

    Updated 2025 version

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ This dataset lists monthly average home prices in various US cities from 2008 to
 
 Real estate and home prices are things that are constantly on my mind as I try to build up a real
 estate portfolio. Having quick access to home prices by city is useful in making future plans. This
-program allows me to analyze the data for patterns and thereby better understand house prices. I 
-can use this information and cross-reference it with other data from the time period to draw 
-conclusions about the housing market in general and make predicitons for the future.
+program allows me to analyze the data for patterns and thereby better understand house prices.
 
-[Software Demo Video](https://youtu.be/qSQuszIbzCI)
+[Software Demo Video](https://youtu.be/qgZZA-0HFN0)
 
 # Data Analysis Results
 1. Which cities can the average american afford, based off an average annual income of 54,000 
    and a 20,000 down payment allowing for a 186,187 budget?
-   I filtered the data to only show cities with average home price under the budget.
+   I made it possible to filter the data based on a given price.
 2. What are the 10 least expensive cities to buy a home in? 
    My program uses sorting to show the 10 least expensive cities. 
+3. What is the 2020 average of home prices in a specific city? 
+   I can filter the data based on a given city name.
 
 
 # Development Environment
--- main.py
+++ main.py
@@ -1,38 +1,52 @@
+# Code by Sara Nuss
+
+# A tool to analyze housing market data using pandas.
+# This program uses a dataset of house prices by city
 
 import pandas as pd
 
 
 def avg_prices(data):
-    data["Average"] = (data["2020-01"] + data["2020-02"]) / 2
-
+    data["Average2020"] = (data["2020-01"] + data["2020-02"]) / 2
 
 def sort(data):
-    sorted_data = data.sort_values(by='Average')
-    return sorted_data
+    sorted_data = data.sort_values(by='Average2020')
+    print(sorted_data.head(10))
 
-def filter_data(data, value):
-    filtered_data = data[data.Average < value]
-    return filtered_data
-
+def filter_data(data):
+    value = input("Enter price: ")
+    direction = input("Show data greater than or less than entered price? Enter 'g' or 'l' ")
+    filtered_data = None
+    if direction == 'g':
+        filtered_data = data[data.Average2020 > float(value)]
+    elif direction == 'l':
+        filtered_data = data[data.Average2020 < float(value)]
+    print(filtered_data)
 
 def display_menu():
-    selection = input("Select 1\n"
-                      "0 - Filter\n"
+    user_selection = input("Select 1\n"
+                      "0 - Filter by house price\n"
                       "1 - Sort\n"
                       "2 - View table\n"
+                      "3 - Find City\n"
                       "- ")
-    return selection
+    return user_selection
 
+def find_city(sales_prices):
+    user_location = input("City: ")
+    sp_city = sales_prices[(sales_prices["RegionName"] == user_location)]
+    print(sp_city)
 
-sales_prices = pd.read_csv("Sale_Prices_City.csv")
-avg_prices(sales_prices)
+sales_prices_unedited = pd.read_csv("Sale_Prices_City.csv")
+avg_prices(sales_prices_unedited)
+sales_prices = sales_prices_unedited[["RegionName", "StateName", "SizeRank", "Average2020"]];
 selection = display_menu()
 if selection == '0':
-    filtered_data = filter_data(sales_prices, 186871)
-    print(filtered_data)
+    filter_data(sales_prices)
 elif selection == '1':
-    sorted_data = sort(sales_prices)
-    print(sorted_data.head(10))
+    sort(sales_prices)
 elif selection == '2':
     print(sales_prices)
+elif selection == '3':
+    find_city(sales_prices)
 #print(sales_prices)
